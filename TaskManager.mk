77a39945de2d2a3e4f4bc274fe1f0ee88d131f12
yassine <yassinessafi2002@gmail.com>
2025-01-04 15:01:08 +0100

    :green_heart: OK

--- TaskManager.mk
+++ TaskManager.mk
@@ -1,25 +1,20 @@
-# Fichiers de configuration Docker Compose
 DEV_CONFIG = docker-compose.yml
 PROD_CONFIG = docker-compose.prod.yml
 
-# Commandes pour l'environnement de développement
 dev-up:
 	docker compose -f $(DEV_CONFIG) up
 dev-down:
 	docker compose -f $(DEV_CONFIG) down
 
-# Commandes pour l'environnement de production
 prod-up:
 	docker compose -f $(PROD_CONFIG) up
 prod-down:
 	docker compose -f $(PROD_CONFIG) down
 
-# Commande pour arrêter tous les conteneurs
 stop-all:
 	docker compose -f $(DEV_CONFIG) down
 	docker compose -f $(PROD_CONFIG) down
 
-# Gestion des branches avec Twgit
 start-fix:
 	twgit hotfix start
 end-fix:
